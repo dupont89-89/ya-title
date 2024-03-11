e6f4dcb3e536355d625ca7843a6de7bc8e2fab88
dupont89-89 <dupont89-89@yandex.ru>
2024-03-12 00:11:39 +0500

    hak-no

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const apiKey = process.env.REACT_APP_API_KEY;
+const yaCatalog = process.env.REACT_APP_YANDEX_CATALOG;
 
 // Middleware для разрешения CORS
 app.use((req, res, next) => {
@@ -26,8 +30,7 @@ app.use(
     target: "https://yandex.ru",
     changeOrigin: true,
     pathRewrite: {
-      "^/api":
-        "/search/xml?folderid=b1gemoaetdknc1hodeee&filter=moderate&lr=213&l10n=ru",
+      "^/api": `/search/xml?folderid=${yaCatalog}&filter=moderate&lr=213&l10n=ru`,
     },
     onProxyReq(proxyReq, req, res) {
       // Добавьте логирование для просмотра адреса запроса и установленных заголовков
@@ -35,10 +38,7 @@ app.use(
       console.log("Request Headers:", proxyReq._headers);
 
       // Добавьте любые заголовки, которые могут потребоваться для аутентификации
-      proxyReq.setHeader(
-        "Authorization",
-        "Api-Key AQVN0qCGDXIufvoikxRcR0ImZiaPAldgTfDJjf-f"
-      );
+      proxyReq.setHeader("Authorization", `Api-Key ${apiKey}`);
     },
   })
 );
