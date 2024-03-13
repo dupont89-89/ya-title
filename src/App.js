d57d059525d816b6d89dfbd3583f5e970dcbcd77
dupont89-89 <dupont89-89@yandex.ru>
2024-03-14 03:18:09 +0500

    lemitizaciya

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import ToolSidebar from "./Sidebar/ToolSidebar";
 import ApiSendYaSearch from "./api-ya-search/ApiSendYaSearch";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="blockOsn">
-        <ApiSendYaSearch />
+        <div className="sectionGridSK">
+          <aside>
+            <ToolSidebar />
+          </aside>
+          <article>
+            <Routes>
+              <Route path="/" element={<ApiSendYaSearch />} />
+            </Routes>
+          </article>
+        </div>
       </div>
     </div>
   );
