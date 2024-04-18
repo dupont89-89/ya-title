5d2695c0848aa6d3f47929bd3681b7583a052446
dupont89-89 <dupont89-89@yandex.ru>
2024-04-18 23:15:46 +0500

    modal-robo

--- src/config.dev.js
+++ src/config.dev.js
@@ -3,6 +3,8 @@ const config = {
   REACT_APP_SERVER_URL: "http://localhost:8080",
   REACT_APP_FRONT_URL: "http://localhost:3000",
   debug: true,
+  ROBOKASSA_PASSWORD_1: "JaA4qkHaB6rCa6tq9Q7G",
+  ROBOKASSA_SHOP_NAME: "ptahini",
 };
 
 module.exports = config;
