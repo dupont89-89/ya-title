c075dc2d001f3d3d2deb203981ef0e5fb04e305f
dupont89-89 <dupont89-89@yandex.ru>
2024-04-19 10:06:57 +0500

    robo-invoic

--- src/BalancePage/BalancePageContainer.jsx
+++ src/BalancePage/BalancePageContainer.jsx
@@ -3,12 +3,20 @@ import BalancePage from "./BalancePage";
 import { connect } from "react-redux";
 
 function BalancePageContainer(props) {
-  return <BalancePage money={props.money} />;
+  return (
+    <BalancePage
+      email={props.email}
+      userId={props.userId}
+      money={props.money}
+    />
+  );
 }
 
 const mapStateToProps = (state) => {
   return {
     money: state.user.dataUser.money,
+    userId: state.user.dataUser.userId,
+    email: state.user.dataUser.email,
   };
 };
 const mapDispatchToProps = {};
