d3bc13362a5f36131206f1b769197ca6c11f51a7
dupont89-89 <dupont89-89@yandex.ru>
2024-04-30 01:52:31 +0500

    add-money

--- src/config.dev.js
+++ src/config.dev.js
@@ -4,6 +4,7 @@ const config = {
   REACT_APP_FRONT_URL: "http://localhost:3000",
   debug: true,
   ROBOKASSA_PASSWORD_1: "JaA4qkHaB6rCa6tq9Q7G",
+  ROBOKASSA_PASSWORD_2: "lGbx8N0br7XxbA10JVMJ",
   ROBOKASSA_SHOP_NAME: "ptahini",
   ROBOKASSA_TEST: "1",
 };
---
+++ src/pay/test-pogert/TestButtonPayRobokassaAnswer.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+const md5 = require("md5");
+
+let config;
+
+if (process.env.NODE_ENV === "development") {
+  config = require("./../../config.dev");
+} else {
+  config = require("./../../config.prod");
+}
+
+const merchant_login = config.ROBOKASSA_SHOP_NAME;
+const password_2 = config.ROBOKASSA_PASSWORD_2;
+const IsTest = config.ROBOKASSA_TEST;
+
+const TestButtonPayRobokassaAnswer = () => {
+  const sendTestRequest = () => {
+    // Параметры запроса
+    const signatureValue = md5(`${merchant_login}:105:769726:${password_2}`);
+
+    const params = {
+      OutSum: "105.00",
+      InvId: "769726",
+      SignatureValue: signatureValue,
+      IsTest: IsTest,
+    };
+
+    // Преобразование параметров в строку запроса
+    const queryString = Object.keys(params)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(params[key])
+      )
+      .join("&");
+
+    // URL для отправки запроса
+    const url = "http://localhost:8080/api/pay/success-payment?" + queryString;
+    debugger;
+
+    // Отправка GET запроса
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Успешный ответ от сервера:", data);
+        // Здесь можно обработать успешный ответ от сервера, если необходимо
+      })
+      .catch((error) => {
+        console.error("Ошибка при отправке запроса:", error);
+        // Здесь можно обработать ошибку при отправке запроса, если необходимо
+      });
+  };
+
+  return (
+    <div>
+      <h2>Инциализировать оплату от Робокассы</h2>
+      <button onClick={sendTestRequest}>Отправить запрос</button>
+    </div>
+  );
+};
+
+export default TestButtonPayRobokassaAnswer;
